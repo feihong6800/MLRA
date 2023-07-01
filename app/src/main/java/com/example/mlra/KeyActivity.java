3279bd8245c2002e023d610523aee542f2484117
Fei Hong <feihong0806@gmail.com>
2023-07-01 09:47:19 +0800

    Update KeyActivity

--- app/src/main/java/com/example/mlra/KeyActivity.java
+++ app/src/main/java/com/example/mlra/KeyActivity.java
@@ -1,14 +1,38 @@
 package com.example.mlra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class KeyActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key);
+
+        toolbar = (Toolbar) findViewById(R.id.key_toolbar);
+
+
+        //Toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_left));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
